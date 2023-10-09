db3494391f29cc42576203067e0d445bd068e185
muyildirim <mehmetuguryldrm@gmail.com>
2023-10-10 00:20:58 +0300

    Degisken isimleri turkcelestirildi, duzen sirasi istenilen tarzda oldu.

--- src/patikadev/bmi/Main.java
+++ src/patikadev/bmi/Main.java
@@ -17,15 +17,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(in);
 
-        out.print("Lutfen Kilonuzu Giriniz -kg : ");
-        double kilo = scanner.nextDouble();
-
-        out.print("Lutfen Boyunuzu Giriniz -m : ");
+        out.print("Lutfen boyunuzu (metre cinsinde) giriniz : ");
         double boy = scanner.nextDouble();
+
+        out.print("Lutfen kilonuzu giriniz : ");
+        double kilo = scanner.nextDouble();
 
         double kitleIndex = kilo / (Math.pow(boy,2));
 
-        out.println("Vucut Kitle Indexiniz : " + kitleIndex);
+        out.println("Vucut Kitle Indeksiniz : " + kitleIndex);
 
     }
 }
