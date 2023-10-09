df2b0a7ec4b8ac4e3e18a95c2dc57e7734c3efbe
muyildirim <mehmetuguryldrm@gmail.com>
2023-10-10 00:14:13 +0300

    Patika.dev ilk odev tamamlanmistir.

---
+++ src/patikadev/bmi/Main.java
@@ -0,0 +1,31 @@
+package patikadev.bmi;
+
+import java.util.Scanner;
+
+import static java.lang.System.*;
+
+/**
+ * The Main program implements an application that
+ * simply displays person's BMI (Body Mass Index).
+ *
+ * @author Mehmet Ugur Yildirim - muyildirim
+ * @version 0.0.1
+ * @since 2023-10-10
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(in);
+
+        out.println("Lutfen Kilonuzu Giriniz -kg : ");
+        double kilo = scanner.nextDouble();
+
+        out.println("Lutfen Boyunuzu Giriniz -m : ");
+        double boy = scanner.nextDouble();
+
+        double kitleIndex = kilo / (Math.pow(boy,2));
+
+        out.println("Vucut Kitle Indexiniz" + kitleIndex);
+
+    }
+}
